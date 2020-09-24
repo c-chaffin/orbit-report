bc19317d9888e86f38d3a83e81129d7de6e468ee
Christopher Chaffin <christopher.chaffin@gmail.com>
2020-09-24 13:59:48 -0500

    completed part 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,11 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,9 +25,21 @@ export class AppComponent {
             let satellite: object = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
           }
- 
+          this.displayList = this.sourceList.slice(0);
+          
        }.bind(this));
     }.bind(this));
- 
- }
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name: string = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+  }
 }
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -6,12 +6,25 @@ import { Satellite } from '../satellite';
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+
+
 export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sort(column: string): void {
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+      if(a[column] < b[column]) {
+         return -1;
+      } else if (a[column] > b[column]) {
+         return 1;
+      }
+      return 0;
+   });
   }
 
 }
